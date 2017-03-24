efdbc5a16245a8d4a451479825b3afae884dcea6
id0tfqh <id0tfqh@users.noreply.github.com>
2017-03-24 08:05:01 +0300

    Update check-ip.py

--- check-ip.py
+++ check-ip.py
@@ -21,3 +21,8 @@ command = command + addr
 os.system('ssh -24xCt tech@base221 sudo {0}'.format(command))
 os.system('ssh -24xCt tech@base222 sudo {0}'.format(command))
 os.system('ssh -24xCt tech@base241 sudo {0}'.format(command))
+
+def check(command):
+	os.system('ssh -24xCt denis@{0} {1} {2}'.format('echo21','sudo',command))
+p1 = threading.Thread(target=check, name="t1", args=command)
+p1.start()
